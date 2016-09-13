a9ddd5a6565d1d5ca58db3ccd9b7f52e8bf2a153
coolguy84 <5801610@FPSMA.net>
2016-09-13 09:42:09 -0400

    lab 03
    first thing

---
+++ lab03v100Young.py
@@ -0,0 +1,26 @@
+# Lab03v100Young.py
+# The First Program
+# This is the Student Starting version of Lab03.
+#
+print("REQUIRED FOR 80 & 100 POINT VERSIONS")   
+print("()")
+print("*******************************")
+print("Lab 03A, THE FIRST PROGRAM")
+print("80 POINT VERSION")
+print("BY COOPER YOUNG")
+print("*******************************")
+print("()")
+print("A poet I am not")
+print("Don't claim to be")
+print("But what I wrote")
+print("Did come from me")
+print ("")
+print ("")
+print("REQUIRED FOR 100 POINT VERSIONS") 
+print (3 + 10)
+print (8 - 1)
+print (3 * 10)
+print (10 / 3)
+print (10 // 3)
+print (10 % 3)
+print (10 ** 3)
